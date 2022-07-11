344c3266acfa5e10129d33a3bfa7aa9f0f9cd5d6
dishantmalik <dishantmalik@gmail.com>
2022-07-11 11:05:54 -0500

    Update SubstitutionCipher.py

--- SubstitutionCipher.py
+++ SubstitutionCipher.py
@@ -1,21 +1,23 @@
-# Take the input string:
-input_ = str(input('Enter the string > '))
+if __name__ == '__main__':
+	
+	# Take the input string:
+	input_ = str(input('Enter the string > '))
 
-# Take the input n:
-rotateBy = int(input("Enter the rotation 'n' > "))
-substitutedCipher = list()
+	# Take the input n:
+	rotateBy = int(input("Enter the rotation 'n' > "))
+	substitutedCipher = list()
 
-for i in input_:
-	# For lower case characters:
-	if ord(i) >= 97 and ord(i) <=122:
-		substitutedCipher.append(chr((ord(i) + rotateBy - 97) % 26 + 97))
+	for i in input_:
+		# For lower case characters:
+		if ord(i) >= 97 and ord(i) <=122:
+			substitutedCipher.append(chr((ord(i) + rotateBy - 97) % 26 + 97))
 
-	# For upper case characters:
-	elif ord(i) >= 65 and ord(i) <= 90:
-		substitutedCipher.append(chr((ord(i) + rotateBy - 65) % 26 + 65))
+		# For upper case characters:
+		elif ord(i) >= 65 and ord(i) <= 90:
+			substitutedCipher.append(chr((ord(i) + rotateBy - 65) % 26 + 65))
 
-	# For all other:
-	else:
-		substitutedCipher.append(i)
+		# For all other:
+		else:
+			substitutedCipher.append(i)
 
-print ("Rotated String is: " + ''.join(substitutedCipher))
+	print ("Rotated String is: " + ''.join(substitutedCipher))
